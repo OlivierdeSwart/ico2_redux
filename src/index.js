ef0d984c25a42c2c49d47af34d75e23a1ba365a3
Olivier de Swart <olivierdeswart@gmail.com>
2023-12-20 07:31:20 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
